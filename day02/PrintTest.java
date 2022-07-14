f1a87127a7961dfc1f3672df8cb900947fe13250
MinHoKim <kimm3654@naver.com>
2022-07-14 14:02:43 +0900

    자바 공부 2일차

---
+++ day02/PrintTest.java
@@ -0,0 +1,24 @@
+package day02;
+
+public class PrintTest {
+	
+	public static void main(String[] args) {
+		//한줄 주석
+		
+		/* 주석 : 컴파일러가 읽지 않고 뛰어 넘는다.
+		 * 설명글을 달 때 쓰인다.
+		 * 범위주석
+		 * 
+		 * 개행문자(제어문자)
+		 * \n	: 줄바꿈(line)
+		 * \t	: 간격을 맞춰서 띄우기(tab)
+		 * 
+		 * 
+		 */
+		
+		System.out.print("한동석\n");
+		System.out.println("남자");
+		System.out.println("12월04일");
+		
+	}
+}
