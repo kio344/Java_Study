47faf46f6a0b5a933d6e0e5189fac575979520c0
MinHoKim <105355770+kio344@users.noreply.github.com>
2022-07-14 19:49:56 +0900

    Update Rand.java

--- day31/Rand.java
+++ day31/Rand.java
@@ -1,25 +1,5 @@
 package day31;
-/*
- * JAVA2 팀 프로젝트 팀별 명단
 
-1조
-이호인(조장)
-한수빈
-조형규
-전윤호
-이상묵
-
-2조
-민경우(조장)
-김재윤
-조준형
-최원석
-김정수
-
-조이름
-프로젝트 명
- * 
- */
 import java.util.Random;
 
 public class Rand {
