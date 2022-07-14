f351fb7b50596c12d58600c851469e02351c3598
MinHoKim <kimm3654@naver.com>
2022-07-14 18:39:55 +0900

    자바 공부 6일차

---
+++ day06/WhileTest.java
@@ -0,0 +1,19 @@
+package day06;
+
+public class WhileTest {
+	public static void main(String[] args) {
+//		
+//		int cnt = 0;		
+//		
+//		while(cnt != 10) {
+//			System.out.println(cnt+1+"한동석");
+//			cnt++;
+//		}
+		
+		while(true) {
+			System.out.println("한동석");
+		}
+		
+		
+	}
+}
