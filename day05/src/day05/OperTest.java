ca9555c5a24bd76642984b7ccfe2349aa6724834
MinHoKim <kimm3654@naver.com>
2022-07-14 18:31:50 +0900

    자바 공부 5일차

---
+++ day05/src/day05/OperTest.java
@@ -0,0 +1,18 @@
+package day05;
+
+import javax.swing.JOptionPane;
+
+public class OperTest {
+	public static void main(String[] args) {
+		System.out.println(10 == 20);
+		System.out.println(10 == 10 && 20 > 30);
+		System.out.println(10 == 10 || 20 > 30);
+		System.out.println(!(10 > 30));
+	}
+}
+
+
+
+
+
+
