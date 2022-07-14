a190ba3a312c8d5b2477d3b090badb6dd0b4fc33
MinHoKim <105355770+kio344@users.noreply.github.com>
2022-07-14 19:45:33 +0900

    Update GTest2.java

--- day26/GTest2.java
+++ day26/GTest2.java
@@ -5,13 +5,13 @@ public class GTest2 {
 	public static <T> T function(T data) {
 		T result;
 		if(data instanceof Integer) {
-			result = (T)"Á¤¼ö";
+			result = (T)"ì •ìˆ˜";
 		}else if(data instanceof Double || data instanceof Float) {
-			result = (T)"½Ç¼ö";
+			result = (T)"ì‹¤ìˆ˜";
 		}else if(data instanceof Character) {
-			result = (T)"¹®ÀÚ";
+			result = (T)"ë¬¸ì";
 		}else if(data instanceof String) {
-			result = (T)"¹®ÀÚ¿­";
+			result = (T)"ë¬¸ìì—´";
 		}else {
 			result = null;
 		}
